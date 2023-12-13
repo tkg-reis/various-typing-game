653944ef8a3ec1f82094f0642dbef62a6b349f8f
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2023-12-14 01:18:12 +0900

    add UI and typeAction

--- src/App.js
+++ src/App.js
@@ -8,22 +8,29 @@ import CollectSound from "./audio/audio_correct.mp3";
 import WrongSound from "./audio/audio_wrong.mp3";
 
 const options = [
+  // #1 フォントの追加
   {value : "electroHarmonics", label : "エレクトロハーモニクス"},
   {value : "Dynalight", label : "筆記体"},
-  {value : "Flow Circular", label : "やばさ１"},
-  {value: "Libre Barcode 39" , label : "やばさ２"}
+  {value : "Flow Circular", label : "難読レベル１"},
+  {value: "Libre Barcode 39" , label : "難読レベル２"}
 ];
 
 function App() {
-  const [info, setInfo] = useState("start typing");
-  const [selectedState, setSelectedState] = useState(null);
+  const ph = "フォントが選べます"; 
+  const answer = useRef(true);
   const [error, setError] = useState("");
+  const [info, setInfo] = useState("start typing");
+  const [active, setActive] = useState(false);
+  const [count, setCount] = useState(0);
+
   const [typePlaySound] = useSound(TypeWriterSound , {onend: () =>{
+    // #1点数計算を追加
     console.log("sounds good");
   }});
   const [collectSound] = useSound(CollectSound, {
     onend : () => {
       console.log("collect!");
+      setActive(true);
     }
   });
   const [wrongSound] = useSound(WrongSound, {
@@ -32,43 +39,17 @@ function App() {
     }
   });
 
+
+
 // fontfamilyの選択
-  const ref = useRef(null);
+  const ref = useRef("");
   const ChangeFamily = (val) => {
     ref.current.style.fontFamily = val.value;
   }
   const textareaRef = useRef(null);
-   let currentTextarea = textareaRef.current;
+  let currentTextarea = textareaRef.current;
 
-  const changeHandler = () => {
-    typePlaySound();
-        // typeSound.currentTime = 0;
-        const sentenceArray = document.querySelectorAll('.typeDiplay > span');
-        const arrayValue = currentTextarea.value.split("");
-        let correct = true;
-        console.log(arrayValue);
-        sentenceArray.forEach((charsSpan,index) => {
-            if((arrayValue[index] == null)) {
-                // charsSpan.classList.remove('corrected');
-                // charsSpan.classList.remove('incorrected');
-                correct = false;
-            } else if(charsSpan.innerHTML == arrayValue[index]) {
-                // charsSpan.classList.add('corrected');
-                // charsSpan.classList.remove('incorrected');
-            } else {
-                // charsSpan.classList.add('incorrected');
-                // charsSpan.classList.remove('corrected');
-                wrongSound();
-                // wrongSound.currentTime = 0;
-                correct = false;
-            }
-        });
-        if(correct == true) {
-          collectSound();
-            // correctSound.currentTime = 0;
-            // renderNextSentence();
-        }
-  }
+
   useEffect(() => {
     const getData = async() => {
       try {
@@ -80,48 +61,64 @@ function App() {
       }
     }
     getData();
-  },[]);
+  },[active]);
+
+  const changeHandler = () => {
+    typePlaySound();
+    const sentenceArray = document.querySelectorAll('.typeDiplay > span');
+    const arrayValue = currentTextarea.value.split("");
+
+    answer.current = true;
+
+    sentenceArray.forEach((charsSpan,index) => {
+      if((arrayValue[index] == null)) {
+        charsSpan.classList.remove('corrected');
+        charsSpan.classList.remove('incorrected');
+        answer.current = false;
+      } else if(charsSpan.innerHTML == arrayValue[index]) {
+        charsSpan.classList.add('corrected');
+        charsSpan.classList.remove('incorrected');
+        
+      } else {
+        charsSpan.classList.add('incorrected');
+        charsSpan.classList.remove('corrected');
+        wrongSound();
+        answer.current = false;
+      }
+    });
+    if(answer.current === true ) {
+      collectSound();
+      currentTextarea.value = "";
+      setCount(prev => prev + 1);
+    }
+  }
 
   let spiltTxt = info.split("");
-  let timer = 0;
-  let startTime = 0;
-  let originTime = 60;
-  // function startTimer() {
-  //   timer.innerHTML = originTime;
-  //   startTime = new Date();
-  //   setInterval(() => {
-  //       timer.innerHTML = originTime - getTimerTime();
-  //       if(timer.innerHTML <= 0) TimeUp();
-  //   },1000);
-  // }
-  // function getTimerTime () {
-  //     return Math.floor((new Date() - startTime ) / 1000);
-  // }
-  // function TimeUp() {
-  //     renderNextSentence();
-  // }
-  // renderNextSentence();
-
- 
 
   return (
     <div className='App'>
-      <button onClick={() => ChangeFamily()}>test</button>
-      <div>
+      <h1 className='top'>Various-typing-game</h1>
+      <div className='selectBox'>
         <Select
           options={options}
-          defaultValue={ref}
           onChange={ChangeFamily}
-            />
+          autoFocus
+          placeholder={ph}
+          
+        />
       </div>
-      <h2 className='timer' >{timer !== startTime ? originTime : startTime }</h2>
-        <div className='typeDiplay' ref={ref}>
-          {spiltTxt.map((value, i) => {
-            return <span key={i}>{value}</span>
-          })}
-        </div>
+      <div className='typeDiplay' ref={ref}>
+        {spiltTxt.map((value, i) => {
+          return <span key={i}>{value}</span>
+        })}
+      </div>
       <textarea className='textInput' ref={textareaRef} onChange={changeHandler} autoFocus></textarea>
       <p>{!error ? error : ""}</p>
+      <span className='currentCount'>正解数:{count}</span>
+      <footer>
+        <p>正解すると次の問題に解答できます。</p>
+        <small>&copy; tkg-reis.app</small>
+      </footer>
     </div>
   );
 }
